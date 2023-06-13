308266c5e344765b93d2195f4fa2cd2d4fc91396
yubraja <yubrajadhikari987@gmail.com>
2023-06-13 19:04:10 +0545

    click and message show in bottom menu

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -12,21 +12,31 @@ class _MapScreenState extends State<MapScreen> {
   final _mapController = MapController(
     initMapWithUserPosition: UserTrackingOption(enableTracking: true),
   );
+
+  var markerMap = <String, String>{};
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback ((timeStamp)  { 
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       _mapController.listenerMapLongTapping.addListener(() async {
+        var position = _mapController.listenerMapSingleTapping.value;
 
-        var position= _mapController.listenerMapSingleTapping.value;
+        if (position != null) {
+          await _mapController.addMarker(position,
+              markerIcon: MarkerIcon(
+                icon: Icon(
+                  Icons.pin_drop,
+                  color: Colors.blue,
+                  size: 68,
+                ),
+              ));
 
-        if(position!=null){
-
-          await _mapController.addMarker(position,markerIcon:  MarkerIcon(icon: Icon(Icons.pin_drop,color: Colors.blue,size: 68,),));
-
+          //Add Marker
+          var key = '${position.latitude}_${position.longitude}';
+          markerMap[key] = markerMap.length.toString();
         }
-       }); 
+      });
     });
   }
 
@@ -36,7 +46,6 @@ class _MapScreenState extends State<MapScreen> {
     super.dispose();
     _mapController.dispose();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -78,17 +87,60 @@ class _MapScreenState extends State<MapScreen> {
             icon: Icon(
               Icons.person_pin_circle,
               color: Colors.black,
-              size:48,
-              
+              size: 48,
             ),
           )),
-          onMapIsReady: (isReady) async{
-            if(isReady){
-              await Future.delayed(Duration(seconds: 1),() async{
+          onMapIsReady: (isReady) async {
+            if (isReady) {
+              await Future.delayed(Duration(seconds: 1), () async {
                 await _mapController.currentLocation();
               });
             }
-
+          },
+          onGeoPointClicked: (geoPoint) {
+            var key = '${geoPoint.latitude}_${geoPoint.longitude}';
+            if (geoPoint != null) {
+              showBottomSheet(
+                  context: context,
+                  builder: (context) {
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Expanded(
+                                child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  'position ${markerMap[key]}',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.blue),
+                                ),
+                                const Divider(thickness: 1,),
+                                  Text(
+                                  key,
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.red),
+                                ),
+                              ],
+                            )),
+                            GestureDetector(
+                              onTap: () => Navigator.pop(context),
+                              child: Icon(Icons.clear),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  });
+            }
           },
         ),
       ),
