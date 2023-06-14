ed978847b8a55d21143bf53c523c32655af1cd9a
yubraja <yubrajadhikari987@gmail.com>
2023-06-14 18:00:38 +0545

    model of places

---
+++ lib/models/places.dart
@@ -0,0 +1,25 @@
+
+class Places {
+  String name;
+  double lat;
+  double lang;
+  String image;
+  String description;
+  double? ratings;
+  String? review;
+
+  Places({
+    required this.name,
+    required this.lat,
+    required this.lang,
+    required this.image,
+    required this.description,
+    this.ratings, 
+    this.review,
+  });
+
+
+
+
+
+}
