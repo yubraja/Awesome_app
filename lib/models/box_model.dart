0f02147482caddd9ba89786bf6e2727d590903b9
yubraja <yubrajadhikari987@gmail.com>
2023-06-19 23:01:16 +0545

    image description going on

--- lib/models/box_model.dart
+++ lib/models/box_model.dart
@@ -17,18 +17,38 @@ class BoxModel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.3,
-      width: MediaQuery.of(context).size.width * 0.6,
-      child: Card(
-        child: Column(children: [
-          Stack(
-            children: [
-              Image(image: AssetImage(image)),
-              Text(name)
-            ],
-          )
-        ],),
+    return InkWell(
+      onTap: (){},
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.3,
+        width: MediaQuery.of(context).size.width * 0.6,
+        child: Card(
+          child: Column(children: [
+            Stack(
+              alignment: AlignmentDirectional.bottomCenter,
+              children: [
+                Expanded(
+                  child: Container(
+                    
+                    child: SizedBox(
+                      
+                      height: MediaQuery.of(context).size.height*0.15,
+                      width: double.infinity,
+                      
+                      child: Image(image: AssetImage(image),
+                      fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.black,
+    
+                  child: Text(name,style: TextStyle(color: Colors.white),))
+              ],
+            )
+          ],),
+        ),
       ),
     );
   }
