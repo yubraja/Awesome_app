755b469f0b412d19da3902dafe8a007bf7181b51
yubraja <yubrajadhikari987@gmail.com>
2023-06-20 23:40:10 +0545

    description of each page

--- lib/models/box_model.dart
+++ lib/models/box_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../screens/desciption.dart';
 
 class BoxModel extends StatelessWidget {
   String name;
@@ -6,48 +7,53 @@ class BoxModel extends StatelessWidget {
   String description;
   double? ratings;
 
-  BoxModel(
-      {required this.name,
-      required this.image,
-      required this.description,
-      this.ratings=0,
-      
-    }
-      );
+  BoxModel({
+    required this.name,
+    required this.image,
+    required this.description,
+    this.ratings = 0,
+  });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: (){},
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>Description(name: name,),
+        ),
+      ),
       child: Container(
         height: MediaQuery.of(context).size.height * 0.3,
         width: MediaQuery.of(context).size.width * 0.6,
         child: Card(
-          child: Column(children: [
-            Stack(
-              alignment: AlignmentDirectional.bottomCenter,
-              children: [
-                Expanded(
-                  child: Container(
-                    
-                    child: SizedBox(
-                      
-                      height: MediaQuery.of(context).size.height*0.15,
-                      width: double.infinity,
-                      
-                      child: Image(image: AssetImage(image),
-                      fit: BoxFit.cover,
+          child: Column(
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.bottomCenter,
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.15,
+                        width: double.infinity,
+                        child: Image(
+                          image: AssetImage(image),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Container(
-                  color: Colors.black,
-    
-                  child: Text(name,style: TextStyle(color: Colors.white),))
-              ],
-            )
-          ],),
+                  Container(
+                      color: Colors.black,
+                      child: Text(
+                        name,
+                        style: TextStyle(color: Colors.white),
+                      ))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/desciption.dart
+++ lib/screens/desciption.dart
@@ -1,2 +1,18 @@
 import 'package:flutter/material.dart';
 
+class Description extends StatelessWidget {
+
+  
+  String name;
+
+  Description({ required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      child: Text(name),
+
+    );
+  }
+}
